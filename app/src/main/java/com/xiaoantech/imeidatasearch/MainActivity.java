b3c1f18b5dd87a60829ae9a364364746af99973b
tanghc10 <738433477@qq.com>
2017-03-08 14:30:50 +0800

    增加了对错误码的判断

--- app/src/main/java/com/xiaoantech/imeidatasearch/MainActivity.java
+++ app/src/main/java/com/xiaoantech/imeidatasearch/MainActivity.java
@@ -63,60 +63,111 @@ public class MainActivity extends AppCompatActivity {
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onHttpGetEvent(HttpGetEvent event){
-        try {
-            JSONObject result = new JSONObject(event.getResultStr());
-            String IMEI = result.getString("imei");
-            TextView textView = (TextView)findViewById(R.id.txt_Imei);
-            textView.setText(IMEI);
-
-            int version = result.getInt("version");
-            String version_a = String.valueOf(version/65536);
-            String version_b = String.valueOf((version%65536)/256);
-            String version_c = String.valueOf(version%256);
-            String Version = version_a + '.' + version_b + '.' + version_c;
-            textView = (TextView)findViewById(R.id.txt_version);
-            textView.setText(Version);
-
-            Long time  = result.getLong("timestamp") * 1000;
-            String date = new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new java.util.Date(time));
-            textView = (TextView)findViewById(R.id.txt_time);
-            textView.setText(date);
-
-            String latANDlon = Double.toString(result.getDouble("latitude")) + "," +  Double.toString(result.getDouble("longitude"));
-            textView = (TextView)findViewById(R.id.txt_latANDlon);
-            textView.setText(latANDlon);
-
-            String Course = Integer.toString(result.getInt("course"));
-            textView = (TextView)findViewById(R.id.txt_course);
-            textView.setText(Course);
-
-            String Speed = Integer.toString(result.getInt("speed"));
-            textView = (TextView)findViewById(R.id.txt_speed);
-            textView.setText(Speed);
-
-            String GSM = Integer.toString(result.getInt("GSM"));
-            textView = (TextView)findViewById(R.id.txt_GSM);
-            textView.setText(GSM);
-
-            String MAXGSM = Integer.toString(result.getInt("MAXGSM"));
-            textView = (TextView)findViewById(R.id.txt_MAXGSM);
-            textView.setText(MAXGSM);
-
-            String Voltage = Integer.toString(result.getInt("voltage"));
-            textView = (TextView)findViewById(R.id.txt_voltage);
-            textView.setText(Voltage);
-
-            int state = result.getInt("state");
-            String State = "";
-            if (state == 1){
-                State = "online";
-            }else if(state == 2){
-                State = "offline";
+        if (event.getResultStr().indexOf("code") != -1){
+            try {
+                JSONObject result = new JSONObject(event.getResultStr());
+                TextView textView = (TextView)findViewById(R.id.txt_state);
+                int code = result.getInt("code");
+                if (code == 100){
+                    textView.setText("服务器内部错误");
+                }else if (code == 101){
+                    textView.setText("请求无IMEI");
+                }else if (code == 102){
+                    textView.setText("无请求内容");
+                }else if (code == 103){
+                    textView.setText("请求内容错误");
+                }else if (code == 104){
+                    textView.setText("请求URL错误");
+                }else if (code == 105){
+                    textView.setText("请求范围过大");
+                }else if (code == 106){
+                    textView.setText("simcom服务器无响应");
+                }else if (code == 107){
+                    textView.setText("simcom服务器不在线");
+                }else if (code == 108){
+                    textView.setText("设备无响应");
+                }else if (code == 109){
+                    textView.setText("未登录");
+                }else if (code == 110){
+                    textView.setText("操作设备不成功");
+                }
+            }catch (JSONException e){
+                e.printStackTrace();
             }
-            textView = (TextView)findViewById(R.id.txt_state);
-            textView.setText(State);
-        }catch (JSONException e){
-            e.printStackTrace();
+            TextView textView = (TextView)findViewById(R.id.txt_Imei);
+            textView.setText("");
+            textView = (TextView)findViewById(R.id.txt_version);
+            textView.setText("");
+            textView = (TextView)findViewById(R.id.txt_time);
+            textView.setText("");
+            textView = (TextView)findViewById(R.id.txt_latANDlon);
+            textView.setText("");
+            textView = (TextView)findViewById(R.id.txt_course);
+            textView.setText("");
+            textView = (TextView)findViewById(R.id.txt_speed);
+            textView.setText("");
+            textView = (TextView)findViewById(R.id.txt_GSM);
+            textView.setText("");
+            textView = (TextView)findViewById(R.id.txt_MAXGSM);
+            textView.setText("");
+            textView = (TextView)findViewById(R.id.txt_voltage);
+            textView.setText("");
+        }else{
+            try {
+                JSONObject result = new JSONObject(event.getResultStr());
+                String IMEI = result.getString("imei");
+                TextView textView = (TextView)findViewById(R.id.txt_Imei);
+                textView.setText(IMEI);
+
+                int version = result.getInt("version");
+                String version_a = String.valueOf(version/65536);
+                String version_b = String.valueOf((version%65536)/256);
+                String version_c = String.valueOf(version%256);
+                String Version = version_a + '.' + version_b + '.' + version_c;
+                textView = (TextView)findViewById(R.id.txt_version);
+                textView.setText(Version);
+
+                Long time  = result.getLong("timestamp") * 1000;
+                String date = new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new java.util.Date(time));
+                textView = (TextView)findViewById(R.id.txt_time);
+                textView.setText(date);
+
+                String latANDlon = Double.toString(result.getDouble("latitude")) + "," +  Double.toString(result.getDouble("longitude"));
+                textView = (TextView)findViewById(R.id.txt_latANDlon);
+                textView.setText(latANDlon);
+
+                String Course = Integer.toString(result.getInt("course"));
+                textView = (TextView)findViewById(R.id.txt_course);
+                textView.setText(Course);
+
+                String Speed = Integer.toString(result.getInt("speed"));
+                textView = (TextView)findViewById(R.id.txt_speed);
+                textView.setText(Speed);
+
+                String GSM = Integer.toString(result.getInt("GSM"));
+                textView = (TextView)findViewById(R.id.txt_GSM);
+                textView.setText(GSM);
+
+                String MAXGSM = Integer.toString(result.getInt("MAXGSM"));
+                textView = (TextView)findViewById(R.id.txt_MAXGSM);
+                textView.setText(MAXGSM);
+
+                String Voltage = Integer.toString(result.getInt("voltage"));
+                textView = (TextView)findViewById(R.id.txt_voltage);
+                textView.setText(Voltage);
+
+                int state = result.getInt("state");
+                String State = "";
+                if (state == 1){
+                    State = "online";
+                }else if(state == 2){
+                    State = "offline";
+                }
+                textView = (TextView)findViewById(R.id.txt_state);
+                textView.setText(State);
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
         }
     }
 
@@ -125,5 +176,4 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = (TextView)findViewById(R.id.txt_Imei);
         textView.setText("Nosub");
     }
-
 }
